49daf10f56350e97c75adfc272f27b1de52d71a8
DevThompsxn <thompsonilerioluwa@gmail.com>
2026-03-08 21:34:48 +0100

    console.log results

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ const bmi = weightKg / heightSquared;
 isUnderWeight = bmi < 18.5;
 isNormalWeight = bmi >= 18.5 && bmi < 25;
 isOverWeight = bmi >= 25;
+
+isHighRisk = isOverWeight || weightKg > 90;
+
+console.log("============= BMI HEALTH REPORT ============");
+console.log("Name:", personName);
+console.log("BMI:", bmi.toFixed(2));
+console.log("Underweight:", isUnderWeight);
+console.log("Normal Weight:", isNormalWeight);
+console.log("Overweight:", isOverWeight);
+console.log("High Risk Alert:", isHighRisk);
