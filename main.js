828a1e690206315c6fa86dea8e9ca81f00ec7d82
DevThompsxn <thompsonilerioluwa@gmail.com>
2026-03-08 21:17:38 +0100

    assigned variables

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let personName = "Michael Oliver";
-weightKg = 80;
-heightM = 1.80
+let weightKg = 80;
+let heightM = 1.80
+
+const heightSquared = heightM**2;
+const bmi = weightKg / heightSquared;
+
+isUnderWeight = bmi < 18.5;
+isNormalWeight = bmi >= 18.5 && bmi < 25;
+isOverWeight = bmi >= 25;
